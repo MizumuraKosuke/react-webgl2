84f76edce2a75938e48518d60269c89ff5cd6052
Mizumura Kosuke <kosuke@soundonlive.com>
2021-07-31 23:36:28 +0900

    fixed config

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,20 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
-declare module 'glslify' {
-  const func: (src: string) => string
-  export default func
-}
-declare module '*.glsl' {
-  const src: string
-  export default src
-}
-declare module '*.vert' {
-  const src: string
-  export default src
-}
-declare module '*.frag' {
-  const src: string
-  export default src
-}
--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ module.exports = {
     conf.module.rules.push({
       test: /\.(glsl|frag|vert)$/,
       exclude: /node_modules/,
-      use: [ 'glslify-import-loader', 'raw-loader', 'glslify-loader' ],
+      use: [
+        'glslify-import-loader',
+        'raw-loader',
+        'glslify-loader',
+      ],
     })
     return conf
   },
