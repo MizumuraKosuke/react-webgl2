6390d9eb0c80476026393e7a793b9ef4b47e3823
Mizumura Kosuke <kosuke@soundonlive.com>
2021-08-01 00:41:03 +0900

    added vao

--- src/components/canvas/ch02_01_square/index.tsx
+++ src/components/canvas/ch02_01_square/index.tsx
@@ -9,10 +9,8 @@ const Canvas = () => {
   const canvas = createRef<HTMLCanvasElement>()
   const gl = useRef<WebGL2RenderingContext | null>()
   const program = useRef<WebGLProgram | null>()
-  const squareVertexBuffer = useRef<WebGLBuffer | null>()
-  const squareIndexBuffer = useRef<WebGLBuffer | null>()
   const indices = useRef<number[]>([])
-  const aVertexPosition = useRef<number>(0)
+  const squareVAO = useRef<WebGLVertexArrayObject | null>()
 
   const { setProgram } = useSteUp()
 
@@ -27,27 +25,12 @@ const Canvas = () => {
     }
 
     setProgram(vt, fg, gl.current, program.current)
-
-    aVertexPosition.current = gl.current.getAttribLocation(program.current, 'aVertexPosition')
   }
 
   const initBuffers = () => {
-    if (!gl.current) {
+    if (!gl.current || !program.current) {
       return
     }
-    /*
-      V0                    V3
-      (-0.5, 0.5, 0)        (0.5, 0.5, 0)
-      X---------------------X
-      |                     |
-      |                     |
-      |       (0, 0)        |
-      |                     |
-      |                     |
-      X---------------------X
-      V1                    V2
-      (-0.5, -0.5, 0)       (0.5, -0.5, 0)
-    */
     const vertices = [
       -0.5, 0.5, 0,
       -0.5, -0.5, 0,
@@ -57,51 +40,54 @@ const Canvas = () => {
 
     indices.current = [ 0, 1, 2, 0, 2, 3 ]
 
+    // Create VAO instance
+    squareVAO.current = gl.current.createVertexArray()
+    gl.current.bindVertexArray(squareVAO.current)
+
     // Setting up the VBO
-    squareVertexBuffer.current = gl.current.createBuffer()
-    gl.current.bindBuffer(gl.current.ARRAY_BUFFER, squareVertexBuffer.current)
+    const squareVertexBuffer = gl.current.createBuffer()
+    gl.current.bindBuffer(
+      gl.current.ARRAY_BUFFER,
+      squareVertexBuffer,
+    )
     gl.current.bufferData(
       gl.current.ARRAY_BUFFER,
-      new Float32Array(vertices), gl.current.STATIC_DRAW,
+      new Float32Array(vertices),
+      gl.current.STATIC_DRAW,
     )
 
+    const aVertexPosition = gl.current.getAttribLocation(program.current, 'aVertexPosition')
+    gl.current.enableVertexAttribArray(aVertexPosition)
+    gl.current.vertexAttribPointer(aVertexPosition, 3, gl.current.FLOAT, false, 0, 0)
+
     // Setting up the IBO
-    squareIndexBuffer.current = gl.current.createBuffer()
-    gl.current.bindBuffer(gl.current.ELEMENT_ARRAY_BUFFER, squareIndexBuffer.current)
+    const squareIndexBuffer = gl.current.createBuffer()
+    gl.current.bindBuffer(
+      gl.current.ELEMENT_ARRAY_BUFFER,
+      squareIndexBuffer,
+    )
     gl.current.bufferData(
       gl.current.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(indices.current), gl.current.STATIC_DRAW,
+      new Uint16Array(indices.current),
+      gl.current.STATIC_DRAW,
     )
 
     // Clean
+    gl.current.bindVertexArray(null)
     gl.current.bindBuffer(gl.current.ARRAY_BUFFER, null)
     gl.current.bindBuffer(gl.current.ELEMENT_ARRAY_BUFFER, null)
   }
 
   const draw = () => {
-    if (
-      !gl.current
-      || !squareVertexBuffer.current
-      || !squareIndexBuffer.current
-      || !program.current
-    ) {
+    if (!gl.current || !squareVAO.current) {
       return
     }
 
-    // Clear the scene
     gl.current.clear(gl.current.COLOR_BUFFER_BIT | gl.current.DEPTH_BUFFER_BIT)
     gl.current.viewport(0, 0, gl.current.canvas.width, gl.current.canvas.height)
 
-    // Use the buffers we've constructed
-    gl.current.bindBuffer(gl.current.ARRAY_BUFFER, squareVertexBuffer.current)
+    gl.current.bindVertexArray(squareVAO.current)
 
-    gl.current.vertexAttribPointer(aVertexPosition.current, 3, gl.current.FLOAT, false, 0, 0)
-    gl.current.enableVertexAttribArray(aVertexPosition.current)
-
-    // Bind IBO
-    gl.current.bindBuffer(gl.current.ELEMENT_ARRAY_BUFFER, squareIndexBuffer.current)
-
-    // Draw to the scene using triangle primitives
     gl.current.drawElements(
       gl.current.TRIANGLES,
       indices.current.length,
@@ -109,9 +95,7 @@ const Canvas = () => {
       0,
     )
 
-    // Clean
-    gl.current.bindBuffer(gl.current.ARRAY_BUFFER, null)
-    gl.current.bindBuffer(gl.current.ELEMENT_ARRAY_BUFFER, null)
+    gl.current.bindVertexArray(null)
   }
 
   const init = () => {
@@ -126,6 +110,10 @@ const Canvas = () => {
       alert('Sorry! WebGL is not available')
       return
     }
+
+    const dpr = window.devicePixelRatio || 1
+    gl.current.canvas.width  = canvas.current.clientWidth * dpr
+    gl.current.canvas.height = canvas.current.clientHeight * dpr
 
     initProgram()
     initBuffers()
