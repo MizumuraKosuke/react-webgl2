2f01aaca5b6240270f9214d54c4d4dc77e01d721
Mizumura Kosuke <kosuke@soundonlive.com>
2021-08-09 17:28:36 +0900

    fixed camera word

--- src/hooks/models/camera.ts
+++ src/hooks/models/camera.ts
@@ -4,10 +4,9 @@ import { mat4, vec3, vec4 } from 'gl-matrix'
 interface Hooks {
   type: MutableRefObject<'ORBITING_TYPE' | 'TRACKING_TYPE'>
   position: MutableRefObject<vec3>
-  focus: MutableRefObject<vec3>
-  up: MutableRefObject<vec3>
-  right: MutableRefObject<vec3>
-  normal: MutableRefObject<vec3>
+  upAxis: MutableRefObject<vec3>
+  rightAxis: MutableRefObject<vec3>
+  cameraAxis: MutableRefObject<vec3>
   matrix: MutableRefObject<mat4>
   steps: MutableRefObject<number>
   azimuth: MutableRefObject<number>
@@ -28,18 +27,19 @@ interface Hooks {
 const home = [ 0, 7, 36 ] as vec3
 const useCamera: () => Hooks = () => {
   const type = useRef<'ORBITING_TYPE' | 'TRACKING_TYPE'>('ORBITING_TYPE')
+  
+  const upAxis = useRef(vec3.create())
+  const rightAxis = useRef(vec3.create())
+  const cameraAxis = useRef(vec3.create())
   const position = useRef(home)
-  const focus = useRef(vec3.create())
-
-  const up = useRef(vec3.create())
-  const right = useRef(vec3.create())
-  const normal = useRef(vec3.create())
 
   const matrix = useRef(mat4.create())
 
   const steps = useRef(0)
   const azimuth = useRef(0)
   const elevation = useRef(0)
+
+  // perspective camera option
   const fov = useRef(45)
   const minZ = useRef(0.1)
   const maxZ = useRef(10000)
@@ -63,20 +63,20 @@ const useCamera: () => Hooks = () => {
       mat4.translate(matrix.current, matrix.current, position.current)
     }
 
-    const newRight = vec4.create()
-    vec4.set(newRight, 1, 0, 0, 0)
-    vec4.transformMat4(newRight, newRight, matrix.current)
-    vec3.copy(right.current, newRight as vec3)
+    const newRightAxis = vec4.create()
+    vec4.set(newRightAxis, 1, 0, 0, 0)
+    vec4.transformMat4(newRightAxis, newRightAxis, matrix.current)
+    vec3.copy(rightAxis.current, newRightAxis as vec3)
 
-    const newUp = vec4.create()
-    vec4.set(newUp, 0, 1, 0, 0)
-    vec4.transformMat4(newUp, newUp, matrix.current)
-    vec3.copy(up.current, newUp as vec3)
+    const newUpAxis = vec4.create()
+    vec4.set(newUpAxis, 0, 1, 0, 0)
+    vec4.transformMat4(newUpAxis, newUpAxis, matrix.current)
+    vec3.copy(upAxis.current, newUpAxis as vec3)
 
-    const newNormal = vec4.create()
-    vec4.set(newNormal, 0, 0, 1, 0)
-    vec4.transformMat4(newNormal, newNormal, matrix.current)
-    vec3.copy(normal.current, newNormal as vec3)
+    const newCameraAxis = vec4.create()
+    vec4.set(newCameraAxis, 0, 0, 1, 0)
+    vec4.transformMat4(newCameraAxis, newCameraAxis, matrix.current)
+    vec3.copy(cameraAxis.current, newCameraAxis as vec3)
   }
 
   const setType = (newType: 'ORBITING_TYPE' | 'TRACKING_TYPE') => {
@@ -89,32 +89,26 @@ const useCamera: () => Hooks = () => {
   }
 
   const setAzimuth = (newAzimuth: number) => {
-    azimuth.current += newAzimuth - azimuth.current
-    if (azimuth.current > 360 || azimuth.current < -360) {
-      azimuth.current = azimuth.current % 360
-    }
+    azimuth.current = newAzimuth % 360
     update()
   }
 
   const setElevation = (newElevation: number) => {
-    elevation.current += newElevation - elevation.current
-    if (elevation.current > 360 || elevation.current < -360) {
-      elevation.current = elevation.current % 360
-    }
+    elevation.current = newElevation % 360
     update()
   }
 
   const dolly = (stepIncrement: number) => {
-    const newNormal = vec3.create()
+    const newCameraAxis = vec3.create()
     const newPosition = vec3.create()
-    vec3.normalize(newNormal, normal.current)
+    vec3.normalize(newCameraAxis, cameraAxis.current)
 
     const newStep = stepIncrement - steps.current
 
     if (type.current === 'TRACKING_TYPE') {
-      newPosition[0] = position.current[0] - newStep * newNormal[0]
-      newPosition[1] = position.current[1] - newStep * newNormal[1]
-      newPosition[2] = position.current[2] - newStep * newNormal[2]
+      newPosition[0] = position.current[0] - newStep * newCameraAxis[0]
+      newPosition[1] = position.current[1] - newStep * newCameraAxis[1]
+      newPosition[2] = position.current[2] - newStep * newCameraAxis[2]
     }
     else {
       newPosition[0] = position.current[0]
@@ -149,10 +143,9 @@ const useCamera: () => Hooks = () => {
   return {
     type,
     position,
-    focus,
-    up,
-    right,
-    normal,
+    upAxis,
+    rightAxis,
+    cameraAxis,
     matrix,
     steps,
     azimuth,
